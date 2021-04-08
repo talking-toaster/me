6f937c7a2d31936427f4c29337740ad4d21478bf
talking-toaster <49156890+talking-toaster@users.noreply.github.com>
2021-04-08 21:00:15 +0800

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -1,2 +1,11 @@
 KEY=$PRIV_KEY
-echo "key=$KEY"
+
+echo "hello"
+
+mkdir -p /root/.ssh
+echo "$KEY" > /root/.ssh/id_rsa
+chmod 600 /root/.ssh/id_rsa
+mkdir -p ~/.ssh
+cp /root/.ssh/* ~/.ssh/
+
+GIT_SSH_COMMAND="ssh -v"
