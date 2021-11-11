ec8aea2845afd2b307c58db3e6c3723f29c7ef7e
talking-toaster <49156890+talking-toaster@users.noreply.github.com>
2021-11-11 23:08:14 +0800

    test

--- sync.sh
+++ sync.sh
@@ -11,5 +11,5 @@ mkdir -p  ~/.ssh
 echo "$SSH_PRIVATE_KEY" > ~/.ssh/id_rsa
 chmod 600 ~/.ssh/id_rsa
 
-echo "StrictHostKeyChecking no" >> /etc/ssh/ssh_config
+#echo "StrictHostKeyChecking no" >> /etc/ssh/ssh_config
 
