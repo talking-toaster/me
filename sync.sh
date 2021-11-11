147788e89ffb8dbf9c71f010cfd738292575cd89
talking-toaster <49156890+talking-toaster@users.noreply.github.com>
2021-11-11 22:55:50 +0800

    test

--- sync.sh
+++ sync.sh
@@ -1,6 +1,4 @@
-!#/usr/bush
-
-set -e
+# set -e
 
 KEY=$PRIV_KEY
 
@@ -9,7 +7,12 @@ KEY=$PRIV_KEY
 echo "start"
 pwd
 ls
-chmod 777 /
+chmod 777 /root
 mkdir -p /root/.ssh
 echo "$SSH_PRIVATE_KEY" > /root/.ssh/id_rsa
 chmod 600 /root/.ssh/id_rsa
+
+echo "StrictHostKeyChecking no" >> /etc/ssh/ssh_config
+
+mkdir -p ~/.ssh
+cp /root/.ssh/* ~/.ssh/ 2> /dev/null || true
