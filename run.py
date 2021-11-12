251047b90d408e41b90546e2d8f5e2cff6ec2db1
talking-toaster <49156890+talking-toaster@users.noreply.github.com>
2021-11-12 16:19:08 +0800

    h

--- run.py
+++ run.py
@@ -5,7 +5,8 @@ import json
 access_token = os.environ["GITEE_ACCESS_TOKEN"]
 nodes_str = os.environ["NODES"]
 
-for k,v in json.loads(nodes_str).items:
-    print(k,v)
+nodes = json.loads(nodes_str)
+
+print(nodes)
 
 
